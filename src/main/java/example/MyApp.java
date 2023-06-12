4d634135becff436357b8c56d93233dac8cdae72
Eslam Sife <eng.eslamsife@gmail.com>
2023-06-12 22:52:52 +0300

    test

---
+++ src/main/java/example/MyApp.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.keycloak.services.validation.Validation;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        // Use the Validation class
+        Validation validation = new Validation();
+        // ... do something with the Validation class
+    }
+}
